c19668965952b1fe38feab31b6aabd1c4f803588
Lil5354 <dttthao.5354@gmail.com>
2025-09-23 17:40:09 +0700

    Update layout for Community

---
+++ Controllers/CommunityController.cs
@@ -0,0 +1,18 @@
+﻿// Controllers/CommunityController.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MomExchange.Controllers // Tên namespace của bạn có thể khác
+{
+    public class CommunityController : Controller
+    {
+        // GET: Community
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
